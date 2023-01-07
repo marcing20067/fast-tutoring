a64f772c22ecaf93bca73d0390c54cae2b99159f
Marcin8013 <marcing2067@wp.pl>
2023-01-07 19:20:42 +0100

    Init project

---
+++ src/main/java/com/fasttutoring/FastTutoringApplication.java
@@ -0,0 +1,13 @@
+package com.fasttutoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTutoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastTutoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fasttutoring/FastTutoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasttutoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTutoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
